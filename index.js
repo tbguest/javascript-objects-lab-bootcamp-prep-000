6d43648dd8fa57fb33dbbb707c047f99817a847d
Tristan Guest <tristanguest@gmail.com>
2020-06-17 11:32:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  newObj = Object.assign({}, object, {key: value})
+  newObj = Object.assign({}, object, {[key]: value})
   return newObj
 }
 
