5c29cc1655b714547df6719dc3b7bef450dd05e4
Tristan Guest <tristanguest@gmail.com>
2020-06-17 11:42:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,15 +18,18 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 // console.log(destructivelyUpdateObjectWithKeyAndValue(recipes, 'pie', 'apple'))
 // console.log(recipes)
 
-// function deleteFromObjectByKey(object, key) {
+function deleteFromObjectByKey(object, key) {
   
-// }
+}
+
+console.log(destructivelyDeleteFromObjectByKey(recipes, 'cake'))
+console.log(recipes)
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
 
-console.log(destructivelyDeleteFromObjectByKey(recipes, 'cake'))
-console.log(recipes)
+// console.log(destructivelyDeleteFromObjectByKey(recipes, 'cake'))
+// console.log(recipes)
 
