1c04e6b79fe3cc11f3b453cf6ef9149e1f0d3555
Tristan Guest <tristanguest@gmail.com>
2020-06-17 11:39:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 // }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+  delete object[key]
+  return object
 }
 
