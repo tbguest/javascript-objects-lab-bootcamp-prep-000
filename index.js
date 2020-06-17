21ee785447cbf4ffcf0a3ad00c9452a8f99542d5
Tristan Guest <tristanguest@gmail.com>
2020-06-17 11:34:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return object[key] = value
+  object[key] = value
+  return object
 }
 
 console.log(destructivelyUpdateObjectWithKeyAndValue(recipes, 'pie', 'apple'))
