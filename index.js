f48ad46a289e4d2c1496ef2c5b3ac6401b3687d4
Tristan Guest <tristanguest@gmail.com>
2020-06-17 11:40:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   return object
 }
 
-console.log(destructivelyUpdateObjectWithKeyAndValue(recipes, 'pie', 'apple'))
+console.log(destructivelyDeleteFromObjectByKey(recipes, 'cake', 'apple'))
 console.log(recipes)
 
