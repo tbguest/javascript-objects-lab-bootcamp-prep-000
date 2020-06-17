cbce96e559acf26abe149bcf3ca3afff234ecb68
Tristan Guest <tristanguest@gmail.com>
2020-06-17 11:30:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+  Object.assign({}, object, {key: value})
 }
 
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+// function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   
-}
+// }
 
-function deleteFromObjectByKey(object, key) {
+// function deleteFromObjectByKey(object, key) {
   
-}
+// }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
+// function destructivelyDeleteFromObjectByKey(object, key) {
   
-}
+// }
 
